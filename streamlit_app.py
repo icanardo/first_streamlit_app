14574bdb05cd5b5b954b77672310d6b05ddfc5d9
icanardo <106481804+icanardo@users.noreply.github.com>
2022-05-29 17:35:14 +0200

    Update streamlit_app.py
    header en text toegevoegd

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+
+streamlit.text('Pannenkoek spek')
+streamlit.text('Pannenkoek appel')
+streamlit.text('Pannenkoek naturel')
