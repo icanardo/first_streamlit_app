00be01d1a18fd98875dd27db7bd917549d215ac8
icanardo <106481804+icanardo@users.noreply.github.com>
2022-05-29 18:03:10 +0200

    Update streamlit_app.py
    met voorbeeld keuze

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ streamlit.text('Pannenkoek spinazie 🥗')
 streamlit.header('🍌 Kindermenu met fruit voor Imke 🥝🍇')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-streamlit.multiselect("Kies wat fruit:", list(my_fruit_list.index))
+streamlit.multiselect("Kies wat fruit:", list(my_fruit_list.index),['Avocado','Strawberries])
 streamlit.dataframe(my_fruit_list)
 
 
