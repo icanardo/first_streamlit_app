b25ef5371e6af4c9ab00ad116b4f2cb54420d87f
icanardo <106481804+icanardo@users.noreply.github.com>
2022-05-29 17:38:54 +0200

    Update streamlit_app.py
    Extra regels, met icoontjes

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,8 @@ streamlit.header('Breakfast Menu')
 streamlit.text('Pannenkoek spek')
 streamlit.text('Pannenkoek appel')
 streamlit.text('Pannenkoek naturel')
+streamlit.text('Pannenkoek spinazie 🥗')
+streamlit.text('Pannenkoek kip 🐔')
+streamlit.text('Pannenkoek advocado 🥑')
+streamlit.text('Tosti ham/kaast 🍞')
+   
