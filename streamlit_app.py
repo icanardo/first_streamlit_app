82e9bbd4dce6e21da5bc92171cee08472e803909
icanardo <106481804+icanardo@users.noreply.github.com>
2022-05-29 18:08:13 +0200

    Update streamlit_app.py
    alleen selectie tonen

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,4 +20,8 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 streamlit.multiselect("Kies wat fruit:", list(my_fruit_list.index),['Avocado','Strawberries'])
 streamlit.dataframe(my_fruit_list)
 
+fruits_selected = streamlit.multiselect("Kies wat fruit:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+streamlit.dataframe(fruit_to_show)
+
 
